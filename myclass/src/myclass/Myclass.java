5cb6258050f70c6af0352c6d516cf58c6a42b96e
Gibrando <gibrando.suryabrata04@gmail.com>
2022-12-02 15:29:29 +0700

    aptu

--- myclass/src/myclass/Myclass.java
+++ myclass/src/myclass/Myclass.java
@@ -16,6 +16,7 @@ public class Myclass {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("Angger kiyowo");
+        System.out.println("Sahril cute");
     }
     
 }
