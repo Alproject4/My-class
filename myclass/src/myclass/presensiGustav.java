22aead7573e2629e068a2b1d33cc451c5d725239
gustavpradana542 <gustavpradana542@gmail.com>
2022-12-07 03:29:05 +0700

    Gustavfix

--- myclass/src/myclass/presensiGustav.java
+++ myclass/src/myclass/presensiGustav.java
@@ -18,10 +18,10 @@ public class presensiGustav {
         Scanner sc = new Scanner(System.in);
 
         String[] fak = {"51", "52", "53", "31"};
-        String[] univ = { "4" };
+        String[] univ = {"14"};
         for (int i = 0; i <= 2; i++) {
             do {
-                    
+                //Validasi NIM
                 System.out.println("Presensi ke-" + i);
                 System.out.println("Masukan Nama : ");
                 mhs[i][0] = sc.next();
@@ -29,7 +29,13 @@ public class presensiGustav {
                 mhs[i][1] = sc.next();
                 String nimmhs = mhs[i][1].substring(2, 4);
                 String angkatan = mhs[i][1].substring(0, 2);
+                String valid = mhs[i][1].substring(4, 6);
+                //Valid NIM
+                if (!(valid.equals(univ[0]))) {
+                    System.out.println("Bukan MHS Sadhar");
+                }
                 System.out.println("angkatan : 20" + angkatan);
+                mhs[i][4] = angkatan;
                 System.out.println("" + mhs[i][1].length());
                 System.out.println(fak[2]);
                 if (mhs[i][1].length() >= 10) {
@@ -52,33 +58,48 @@ public class presensiGustav {
                 } else {
                     System.out.println("Anda Bukan Mahasiswa FST");
                 }
-                System.out.println("===PILIH PRESENSI===");
-                System.out.println("1. Hadir");
-                System.out.println("2. Izin");
-                System.out.println("3. Alpha");
-                System.out.println(" ");
-                mhs[i][3] = sc.next();
-                mhs[i][4] += i;
                 System.out.println("=================================");
                 if (!(mhs[i][1].length() == 9)) {
                     System.out.println("Inputan Nim Tidak Sesuai");
                 }
+
+                //Sahril nim duplicat
             } while (!(mhs[i][1].length() == 9));
 
-            System.out.println("Presensi ke-" + i);
-            System.out.println("Masukan Nama : ");
-            mhs[i][0] = sc.next();
-            System.out.println("Masukan NIM : ");
-            mhs[i][1] = sc.next();
+            //Presensi
+            System.out.println("====PILIH PRESENSI====");
+            System.out.println("1. Hadir");
+            System.out.println("2. Izin");
+            System.out.println("3. Alpha");
+            System.out.println(" ");
+            int pilpres = sc.nextInt();
+            if (pilpres == 1) {
+                mhs[i][3] = "Hadir";
+            } else if (pilpres == 2) {
+                mhs[i][3] = "Izin";
+            } else if (pilpres == 3) {
+                mhs[i][3] = "Alpha";
+            }
 
+            String dup = mhs[i][1];
         }
         for (int j = 0; j <= 2; j++) {
-            System.out.println("=================================");
+            System.out.println("==================================");
             System.out.println("Data MHS : ");
             System.out.println("Nama : " + mhs[j][0]);
             System.out.println("NIM : " + mhs[j][1]);
             System.out.println("Fakultas : " + mhs[j][2]);
+            System.out.println("Presensi : " + mhs[j][3]);
+            System.out.println("Angkatan : " + mhs[j][4]);
         }
     }
 
+    public static void presensikontrol() {
+        System.out.println("====PILIH PRESENSI====");
+        System.out.println("1. Hadir");
+        System.out.println("2. Izin");
+        System.out.println("3. Alpha");
+        System.out.println(" ");
+
+    }
 }
